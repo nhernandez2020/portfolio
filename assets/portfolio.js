68e07e2fd6fde1c5322d9958f758b39772cc8e45
nhernandez2020@hotmail.com <Nhernandez2020@hotmail.com>
2020-03-25 01:22:59 -0700

    added assets, updated contact info, indext.html and added portfolio.js

---
+++ assets/portfolio.js
@@ -0,0 +1,14 @@
+//initializing buttons
+$(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton();
+});
+
+// for parallax image, initializing
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+
+// intitializing floating action button
+$(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton();
+});
